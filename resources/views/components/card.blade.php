2c8dd1bc5196668cbc8ef16b98b02f053db20f4c
nawa aripurna <nap.aripurna@gmail.com>
2018-07-27 16:09:05 +0800

    added something

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,5 +1,3 @@
-@extends('layouts.dashboard-app')
-@section('content')
 <div class="row bg-title">
     <div class="col-lg-3 col-md-4 col-sm-4 col-xs-12">
         <h4 class="page-title">Dashboard</h4>
@@ -17,4 +15,3 @@
         </div>
     </div>
 </div>
-@endsection
