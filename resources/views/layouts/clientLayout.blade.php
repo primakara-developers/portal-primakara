23efe06f45cbe91c4569516ab79dea41b15383b3
Nawa Aripurna <nap.aripurna@gmail.com>
2018-10-07 21:23:57 +0800

    merge with client

---
+++ resources/views/layouts/clientLayout.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Portal Primakara</title>
+    <link rel="stylesheet" href="{{ asset('css/clientApp.css') }}">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+  </head>
+  <body>
+
+    @yield('content')
+
+  </body>
+</html>
