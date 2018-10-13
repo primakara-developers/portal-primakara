c6dd728439f195263775b301db406bfdc5044bee
Nawa Aripurna <nap.aripurna@gmail.com>
2018-10-13 19:33:30 +0800

    login page for admin has been editted but not sure if it looks good

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,9 +1,64 @@
 @extends('layouts.authLayout')
+@section('additional-styles')
+<style>
+    body {
+        background-image: url(/images/bege.jpeg);
+        background-repeat: no-repeat;
+        background-size: cover;
+    }
+    .card.primdev-card {
+        border: none;
+        border-radius: 5px;
+        overflow: hidden;
+        box-shadow: 0px 0px 4px -1px;
+    }
+    .form-control.primdev-form {
+        padding: 0 20px;
+        box-shadow: 0 0 1px 0;
+        border-radius: 22px;
+        border: none;
+    }
+    .form-control.primdev-form:focus {
+        outline: none;
+        border: none;
+        box-shadow: 0 0 3px 0 #80bdff;
+    }
+    .btn.primdev-button {
+        padding: 6px 35px;
+        border-radius: 22px;
+    }
+    .form-check-input.primdev-checkbox {
+        display: none;
+        
+    }
+    .form-check-label {
+        padding-left: 25px;
+    }
+    .form-check-label.primdev-checkbox::before {
+        content: "";
+        display: inline-block;
+        height: 20px;
+        width: 20px;
+        position: absolute;
+        border-radius: 5px;
+        border: 1px solid #333;
+        left: 0;
+
+    }
+    .form-check-input.primdev-checkbox:checked + .form-check-label.primdev-checkbox::before {
+        background-image: url('/images/check.svg');
+        background-repeat: no-repeat;
+        background-size: cover;
+        /* background-color: #80bdff; */
+    }
+</style>
+
+@endsection
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
+        <div class="col-md-5 px-md-5">
+            <div class="card primdev-card">
                 <div class="card-header">{{ __('Login') }}</div>
 
                 <div class="card-body">
@@ -11,10 +66,10 @@
                         @csrf
 
                         <div class="form-group row">
-                            <label for="email" class="col-sm-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+                            <label for="email" class="col-12 col-form-label text-left">{{ __('E-Mail Address') }}</label>
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus>
+                            <div class="col-12">
+                                <input placeholder="type your email" id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }} primdev-form" name="email" value="{{ old('email') }}" required autofocus>
 
                                 @if ($errors->has('email'))
                                     <span class="invalid-feedback" role="alert">
@@ -25,10 +80,10 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+                            <label for="password" class="col-12 col-form-label text-left">{{ __('Password') }}</label>
 
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
+                            <div class="col-12">
+                                <input placeholder="type your password"  id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }} primdev-form" name="password" required>
 
                                 @if ($errors->has('password'))
                                     <span class="invalid-feedback" role="alert">
@@ -39,11 +94,11 @@
                         </div>
 
                         <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+                            <div class="col-12">
+                                <div class="form-check pl-0">
+                                    <input class="form-check-input primdev-checkbox" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
 
-                                    <label class="form-check-label" for="remember">
+                                    <label class="form-check-label primdev-checkbox" for="remember">
                                         {{ __('Remember Me') }}
                                     </label>
                                 </div>
@@ -51,19 +106,18 @@
                         </div>
 
                         <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
+                            <div class="col-12 text-center">
+                                <button type="submit" class="btn bg-primary text-white primdev-button">
                                     {{ __('Login') }}
                                 </button>
-
-                                <a class="btn btn-link" href="{{ route('password.request') }}">
-                                    {{ __('Forgot Your Password?') }}
-                                </a>
                             </div>
                         </div>
                     </form>
                 </div>
             </div>
+            <a class="btn btn-link" href="{{ route('password.request') }}">
+                {{ __('Forgot Your Password?') }}
+            </a>
         </div>
     </div>
 </div>
