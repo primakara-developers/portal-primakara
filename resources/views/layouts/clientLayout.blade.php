9a5d45dc54ee12f05f62490dfd45a5c5ed8d1ef6
Wahyu Budi Saputra <wahyubucil@gmail.com>
2018-10-13 17:55:38 +0800

    Config the versioning and sourcemaps

--- resources/views/layouts/clientLayout.blade.php
+++ resources/views/layouts/clientLayout.blade.php
@@ -3,12 +3,13 @@
   <head>
     <meta charset="utf-8">
     <title>Portal Primakara</title>
-    <link rel="stylesheet" href="{{ asset('css/clientApp.css') }}">
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
     <meta name="viewport" content="width=device-width, initial-scale=1">
   </head>
   <body>
     <div class="wrapper">
       @yield('content')
     </div>
+    <script src="{{ mix('js/app.js') }}"></script>
   </body>
 </html>
