bd2ee3964ffea0ff42490fcff68245bc8acf7247
Nawa Aripurna <nap.aripurna@gmail.com>
2018-10-14 16:34:23 +0800

    change layout of auth

--- resources/views/layouts/authLayout.blade.php
+++ resources/views/layouts/authLayout.blade.php
@@ -6,16 +6,18 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous" />
     <meta name="viewport" content="width=device-width, initial-scale=1">
   </head>
+  <style>
+    .navbar-brand:hover {
+        text-decoration: underline;
+    }
+  </style>
   @yield('additional-styles')
   <body>
     <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
         <div class="container">
-            <a class="navbar-brand" href="{{ url('/') }}">
-                {{ config('app.name', 'Laravel') }}
+            <a class="link text-white navbar-brand" href="{{ url('/') }}">
+                Portal Primakara
             </a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                <span class="navbar-toggler-icon"></span>
-            </button>
         </div>
     </nav>
 
