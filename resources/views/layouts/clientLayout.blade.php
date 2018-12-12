c1eeff4758d9fe6970e50fb2967093086519fe78
Dody Prasetyo <prasetyodody17@gmail.com>
2018-12-12 19:32:33 +0800

    adding bootstrap

--- resources/views/layouts/clientLayout.blade.php
+++ resources/views/layouts/clientLayout.blade.php
@@ -5,6 +5,7 @@
     <title>Portal Primakara</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
     <link rel="stylesheet" href="{{ mix('css/app.css') }}">
     @yield('additional-styles')
   </head>
