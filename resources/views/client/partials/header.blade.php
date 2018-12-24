428bd009c2299a791554940703c1b112891ad636
Wahyu Budi Saputra <wahyubucil@gmail.com>
2018-12-24 23:52:39 +0800

    Complete interface of Mobile Nav

--- resources/views/client/partials/header.blade.php
+++ resources/views/client/partials/header.blade.php
@@ -8,9 +8,20 @@
             <a href="" class="site-header__menu-desktop-item">About</a>
             <a href="" class="site-header__menu-desktop-item site-header__menu-desktop-item--with-background">Event</a>
         </nav>
+
         <a href="javascript:void(0)" class="site-header__menu-mobile-icon">
             <div class="site-header__menu-mobile-icon__middle"></div>
         </a>
+
+        <div class="mobile-nav">
+            <a href="javascript:void(0)" class="mobile-nav__close-button">Close</a>
+            <nav class="mobile-nav__menu">
+                <a href="" class="mobile-nav__menu-item mobile-nav__menu-item--active">Home</a>
+                <a href="" class="mobile-nav__menu-item">Story</a>
+                <a href="" class="mobile-nav__menu-item">About</a>
+                <a href="" class="mobile-nav__menu-item mobile-nav__menu-item--with-background">Event</a>
+            </nav>
+        </div>
     </div>
 </header>
 <!-- Header -->
