713af6162d88103994bc43fbde5cccfdba31514b
Wahyu Budi Saputra <wahyubucil@gmail.com>
2018-12-29 16:40:17 +0800

    Complete search content

--- resources/views/client/postList.blade.php
+++ resources/views/client/postList.blade.php
@@ -7,7 +7,14 @@
     <!-- Navigation Bar -->
     <section class="navigation-bar">
         <div class="navigation-bar__container">
-
+            <ul class="navigation-bar__category-list"></ul>
+            <div class="navigation-bar__search">
+                <form action="">
+                    <div class="navigation-bar__search-content">
+                        <input type="text" name="search" placeholder="Search" class="navigation-bar__search-input">
+                    </div>
+                </form>
+            </div>
         </div>
     </section>
 
