5cd2720a2c10c37c12d9b19830ee3b26698b9df7
nawa aripurna <nap.aripurna@gmail.com>
2018-12-29 21:29:36 +0800

    finish post list for category page

--- resources/views/client/postList.blade.php
+++ resources/views/client/postList.blade.php
@@ -11,6 +11,134 @@
 
 
     <!-- Post List Item -->
+    <div class="post">
+        <div class="post__heading">
+            <h1 class="post__heading__title">Intemezzo</h1>
+        </div>
+        <div class="post__list">
+            <div class="post__list__item">
+                <a href="" class="post__list__item__left">
+                    <img class="post__list__item__image" src="{{ asset('images/no1.jpg') }}">
+                </a>
+                <div class="post__list__item__right">
+                    <a href="" class="post__list__item__hoverable">
+                        <h3 class="post__list__item__title">
+                            UKM Dance Primakara Adakan Lari Marathon Keliling Dunia
+                        </h3>
+                        <p class="post__list__item__preview">Happy Sunday From Software Expand in this week's edition of Feedback Loop, we talk about the future of Windows Phone Whether they bla b</p>
+                    </a>
+                    <span class="post__list__item__writter">Susano'o</span>
+                    <span> - </span>
+                    <span class="post__list__item__timestamp">29 February 2019</span>
+                    <br />
+                    <a href="" class="post__list__item__badge">INTERMEZZO</a>
+                </div>
+            </div>
+            <div class="post__list__item">
+                <a href="" class="post__list__item__left">
+                    <img class="post__list__item__image" src="{{ asset('images/no1.jpg') }}">
+                </a>
+                <div class="post__list__item__right">
+                    <a href="" class="post__list__item__hoverable">
+                        <h3 class="post__list__item__title">
+                            UKM Dance Primakara Adakan Lari Marathon Keliling Dunia
+                        </h3>
+                        <p class="post__list__item__preview">Happy Sunday From Software Expand in this week's edition of Feedback Loop, we talk about the future of Windows Phone Whether they bla b</p>
+                    </a>
+                    <span class="post__list__item__writter">Susano'o</span>
+                    <span> - </span>
+                    <span class="post__list__item__timestamp">29 February 2019</span>
+                    <br />
+                    <a href="" class="post__list__item__badge">INTERMEZZO</a>
+                </div>
+            </div><div class="post__list__item">
+                <a href="" class="post__list__item__left">
+                    <img class="post__list__item__image" src="{{ asset('images/no1.jpg') }}">
+                </a>
+                <div class="post__list__item__right">
+                    <a href="" class="post__list__item__hoverable">
+                        <h3 class="post__list__item__title">
+                            UKM Dance Primakara Adakan Lari Marathon Keliling Dunia
+                        </h3>
+                        <p class="post__list__item__preview">Happy Sunday From Software Expand in this week's edition of Feedback Loop, we talk about the future of Windows Phone Whether they bla b</p>
+                    </a>
+                    <span class="post__list__item__writter">Susano'o</span>
+                    <span> - </span>
+                    <span class="post__list__item__timestamp">29 February 2019</span>
+                    <br />
+                    <a href="" class="post__list__item__badge">INTERMEZZO</a>
+                </div>
+            </div><div class="post__list__item">
+                <a href="" class="post__list__item__left">
+                    <img class="post__list__item__image" src="{{ asset('images/no1.jpg') }}">
+                </a>
+                <div class="post__list__item__right">
+                    <a href="" class="post__list__item__hoverable">
+                        <h3 class="post__list__item__title">
+                            UKM Dance Primakara Adakan Lari Marathon Keliling Dunia
+                        </h3>
+                        <p class="post__list__item__preview">Happy Sunday From Software Expand in this week's edition of Feedback Loop, we talk about the future of Windows Phone Whether they bla b</p>
+                    </a>
+                    <span class="post__list__item__writter">Susano'o</span>
+                    <span> - </span>
+                    <span class="post__list__item__timestamp">29 February 2019</span>
+                    <br />
+                    <a href="" class="post__list__item__badge">INTERMEZZO</a>
+                </div>
+            </div><div class="post__list__item">
+                <a href="" class="post__list__item__left">
+                    <img class="post__list__item__image" src="{{ asset('images/no1.jpg') }}">
+                </a>
+                <div class="post__list__item__right">
+                    <a href="" class="post__list__item__hoverable">
+                        <h3 class="post__list__item__title">
+                            UKM Dance Primakara Adakan Lari Marathon Keliling Dunia
+                        </h3>
+                        <p class="post__list__item__preview">Happy Sunday From Software Expand in this week's edition of Feedback Loop, we talk about the future of Windows Phone Whether they bla b</p>
+                    </a>
+                    <span class="post__list__item__writter">Susano'o</span>
+                    <span> - </span>
+                    <span class="post__list__item__timestamp">29 February 2019</span>
+                    <br />
+                    <a href="" class="post__list__item__badge">INTERMEZZO</a>
+                </div>
+            </div><div class="post__list__item">
+                <a href="" class="post__list__item__left">
+                    <img class="post__list__item__image" src="{{ asset('images/no1.jpg') }}">
+                </a>
+                <div class="post__list__item__right">
+                    <a href="" class="post__list__item__hoverable">
+                        <h3 class="post__list__item__title">
+                            UKM Dance Primakara Adakan Lari Marathon Keliling Dunia
+                        </h3>
+                        <p class="post__list__item__preview">Happy Sunday From Software Expand in this week's edition of Feedback Loop, we talk about the future of Windows Phone Whether they bla b</p>
+                    </a>
+                    <span class="post__list__item__writter">Susano'o</span>
+                    <span> - </span>
+                    <span class="post__list__item__timestamp">29 February 2019</span>
+                    <br />
+                    <a href="" class="post__list__item__badge">INTERMEZZO</a>
+                </div>
+            </div><div class="post__list__item">
+                <a href="" class="post__list__item__left">
+                    <img class="post__list__item__image" src="{{ asset('images/no1.jpg') }}">
+                </a>
+                <div class="post__list__item__right">
+                    <a href="" class="post__list__item__hoverable">
+                        <h3 class="post__list__item__title">
+                            UKM Dance Primakara Adakan Lari Marathon Keliling Dunia
+                        </h3>
+                        <p class="post__list__item__preview">Happy Sunday From Software Expand in this week's edition of Feedback Loop, we talk about the future of Windows Phone Whether they bla b</p>
+                    </a>
+                    <span class="post__list__item__writter">Susano'o</span>
+                    <span> - </span>
+                    <span class="post__list__item__timestamp">29 February 2019</span>
+                    <br />
+                    <a href="" class="post__list__item__badge">INTERMEZZO</a>
+                </div>
+            </div>
+        </div>
+    </div>
 
 
     <!-- Pagination -->
