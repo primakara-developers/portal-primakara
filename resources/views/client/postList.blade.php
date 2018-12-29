4ab9bc670a9f8b611e76f802ce20609b6ab762f7
Wahyu Budi Saputra <wahyubucil@gmail.com>
2018-12-29 23:59:48 +0800

    Complete the collapse category

--- resources/views/client/postList.blade.php
+++ resources/views/client/postList.blade.php
@@ -7,14 +7,29 @@
     <!-- Navigation Bar -->
     <section class="navigation-bar">
         <div class="navigation-bar__container">
-            <ul class="navigation-bar__category-list">
-                <li class="navigation-bar__category-list-item"><a href="/information">Information</a></li>
-                <li class="navigation-bar__category-list-item"><a href="/intermezzo">Intermezzo</a></li>
-                <li class="navigation-bar__category-list-item"><a href="/tips-and-trick">Tips & Trick</a></li>
-                <li class="navigation-bar__category-list-item"><a href="/hardware">Hardware</a></li>
-                <li class="navigation-bar__category-list-item"><a href="/software">Software</a></li>
-                <li class="navigation-bar__category-list-item"><a href="/tutorials">Tutorials</a></li>
-            </ul>
+            <div class="navigation-bar__category">
+                <ul class="navigation-bar__category-list">
+                    <li class="navigation-bar__category-list-item"><a href="/information">Information</a></li>
+                    <li class="navigation-bar__category-list-item"><a href="/intermezzo">Intermezzo</a></li>
+                    <li class="navigation-bar__category-list-item"><a href="/tips-and-trick">Tips & Trick</a></li>
+                    <li class="navigation-bar__category-list-item"><a href="/hardware">Hardware</a></li>
+                    <li class="navigation-bar__category-list-item"><a href="/software">Software</a></li>
+                    <li class="navigation-bar__category-list-item"><a href="/tutorials">Tutorials</a></li>
+                </ul>
+            </div>
+            <div class="navigation-bar__category navigation-bar__category--collapse">
+                <ul class="navigation-bar__category-list">
+                    <li class="navigation-bar__category-list-item"><a href="/information">Information</a></li>
+                    <li class="navigation-bar__category-list-item"><a href="/intermezzo">Intermezzo</a></li>
+                    <li class="navigation-bar__category-list-item"><a href="/tips-and-trick">Tips & Trick</a></li>
+                    <li class="navigation-bar__category-list-item"><a href="/hardware">Hardware</a></li>
+                    <li class="navigation-bar__category-list-item"><a href="/software">Software</a></li>
+                    <li class="navigation-bar__category-list-item"><a href="/tutorials">Tutorials</a></li>
+                    <li class="navigation-bar__category-list-item"><a href="/tutorials">Event</a></li>
+                    <li class="navigation-bar__category-list-item"><a href="/tutorials">UKM</a></li>
+                    <li class="navigation-bar__category-list-item"><a href="/tutorials">Announcement</a></li>
+                </ul>
+            </div>
             <div class="navigation-bar__search">
                 <form action="">
                     <div class="navigation-bar__search-content">
