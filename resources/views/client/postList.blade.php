4226e470fe392eb9355799514fc16330d3782b2a
Wahyu Budi Saputra <wahyubucil@gmail.com>
2018-12-30 02:14:10 +0800

    Create category collapse button

--- resources/views/client/postList.blade.php
+++ resources/views/client/postList.blade.php
@@ -7,7 +7,7 @@
     <!-- Navigation Bar -->
     <section class="navigation-bar">
         <div class="navigation-bar__container">
-            <div class="navigation-bar__category">
+            <!-- <div class="navigation-bar__category">
                 <ul class="navigation-bar__category-list">
                     <li class="navigation-bar__category-list-item"><a href="/information">Information</a></li>
                     <li class="navigation-bar__category-list-item"><a href="/intermezzo">Intermezzo</a></li>
@@ -16,7 +16,7 @@
                     <li class="navigation-bar__category-list-item"><a href="/software">Software</a></li>
                     <li class="navigation-bar__category-list-item"><a href="/tutorials">Tutorials</a></li>
                 </ul>
-            </div>
+            </div> -->
             <div class="navigation-bar__category navigation-bar__category--collapse">
                 <ul class="navigation-bar__category-list">
                     <li class="navigation-bar__category-list-item"><a href="/information">Information</a></li>
@@ -29,6 +29,14 @@
                     <li class="navigation-bar__category-list-item"><a href="/tutorials">UKM</a></li>
                     <li class="navigation-bar__category-list-item"><a href="/tutorials">Announcement</a></li>
                 </ul>
+            </div>
+            <div class="navigation-bar__category-collapse-button-container">
+                <a href="javascript:void(0)" class="navigation-bar__category-collapse-button">
+                    <span class="navigation-bar__collapse-button-icon">
+                        <div class="navigation-bar__collapse-button-icon__middle"></div>
+                    </span>
+                    <span class="navigation-bar__collapse-button-text">Pilih Kategori</span>
+                </a>
             </div>
             <div class="navigation-bar__search">
                 <form action="">
@@ -50,5 +58,13 @@
     <!-- Pagination -->
 
 </main>
+
+@endsection
+
+@section('additional-scripts')
+
+<script>
+
+</script>
 
 @endsection
