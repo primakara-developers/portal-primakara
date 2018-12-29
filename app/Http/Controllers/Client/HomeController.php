51569085852abe0309e8833884b79e28b6f29746
dharmasastra <made.betngandang@gmail.com>
2018-12-29 20:17:24 +0800

    integration back-end for headline section

--- app/Http/Controllers/Client/HomeController.php
+++ app/Http/Controllers/Client/HomeController.php
@@ -4,13 +4,19 @@ namespace App\Http\Controllers\Client;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+
 use App\Category;
+use App\Post;
 
 class HomeController extends Controller
 {
     public function index()
     {
     	// Headline Section
+        $headlines = Post::with('categories')
+            ->where('is_headline', 1)
+            ->orderBy('headline_at', 'desc')
+            ->get();
 
         // Informasi Section
         $category = new Category;
@@ -19,6 +25,8 @@ class HomeController extends Controller
 
         $posts = $SingleCategory->posts()->paginate(4);
 
-    	return view('client.home')->with('posts', $posts);
+        return view('client.home')
+            ->with('posts', $posts)
+            ->with('headlines', $headlines);
     }
 }
