e5da79cc8fd821067b981b6462d877e064346fb8
AkhmadRizki <akhmadrizki24@gmail.com>
2018-12-31 07:20:00 +0800

    finish pagination section

--- resources/views/client/postList.blade.php
+++ resources/views/client/postList.blade.php
@@ -15,6 +15,18 @@
 
     <!-- Pagination -->
 
+    <div class="column-pagination">
+        <div class="column-pagination__block">
+            <a href="#" class="column-pagination__block__number column-pagination__block__number--only">1</a>
+            <a href="#" class="column-pagination__block__number column-pagination__block__number--active">2</a>
+            <a href="#" class="column-pagination__block__number">3</a>
+            <a href="#" class="column-pagination__block__number">4</a>
+            <a href="#" class="column-pagination__block__number">5</a>
+            <a href="#" class="column-pagination__block__number">6</a>
+            <a href="#" class="column-pagination__block__number"><span class="column-pagination__block__number--dots">...</span></a>
+        </div>
+    </div>
+
 </main>
 
 @endsection
