0e6f3a61f3e5c8eac5103c5c5971b28fb7b49064
dodypras <prasetyodody17@gmail.com>
2018-12-31 01:01:04 +0800

    adding scroll for long content

--- resources/views/client/postList.blade.php
+++ resources/views/client/postList.blade.php
@@ -20,9 +20,15 @@
     	<li class="breadcrumbs__breadcrumb-item">
     		<div class="breadcrumbs__breadcrumb-item__breadcrumb-item-container">
     			<a class="breadcrumbs__breadcrumb-item__breadcrumb-item-container__breadcrumb-link" href="#">Intermezzo</a>
-    			<span></span>
+    			<span class="breadcrumbs__breadcrumb-item__breadcrumb-item-container__breadcrumb-item-separator"></span>
     		</div>
     	</li>
+        <li class="breadcrumbs__breadcrumb-item">
+            <div class="breadcrumbs__breadcrumb-item__breadcrumb-item-container">
+                <a class="breadcrumbs__breadcrumb-item__breadcrumb-item-container__breadcrumb-link" href="#">Framework Laravel Milik Pras adalah Framework Terburuk Tahun 2018, Anda Pemakainya?</a>
+                <span></span>
+            </div>
+        </li>
     </ul>
 
     <!-- Post List Item -->
