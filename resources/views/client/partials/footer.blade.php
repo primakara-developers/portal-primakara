bb0f835369f62b8ac8eb6547d7db48f52df484b5
Wahyu Budi Saputra <wahyubucil@gmail.com>
2019-01-02 20:26:29 +0800

    Clean up footer

---
+++ resources/views/client/partials/footer.blade.php
@@ -0,0 +1,41 @@
+<!-- footer section -->
+<footer>
+    <!-- footer top -->
+    <div class="footer">
+      <div class="footer__border"></div>
+      <div class="footer__container">
+          <div class="footer__container__logo">
+            <img src="images/primakara.jpg" alt="" class="footer__container__logo__first">
+            <img src="images/primdev.jpg" alt="" class="footer__container__logo__second">
+          </div>
+          <div class="footer__container__information">
+            <div class="footer__container__information__title">INFORMASI</div>
+            <div class="footer__container__information__list"><a href="#">About</a></div>
+            <div class="footer__container__information__list"><a href="#">Story</a></div>
+            <div class="footer__container__information__list"><a href="#">Event</a></div>
+            <div class="footer__container__information__list"><a href="#">Etcetera</a></div>
+          </div>
+          <div class="footer__container__about">
+              <div class="footer__container__about__detail">
+                  <div class="footer__container__about__detail__title">SUMMARY</div>
+                  <div class="footer__container__about__detail__list">
+                      Newspaper Tech Demo is your tech news website. We
+                      provide you with the lastest breaking news and videos
+                      straight from the technology industry provide you with
+                      the lastest breaking.
+                  </div>
+              </div>
+          </div>
+      </div>
+    </div>
+
+    <!-- footer-bottom -->
+    <div class="footer-bottom">
+      <div class="footer-bottom__container">
+        <div class="footer-bottom__container__copyright">
+            &copy;2019 - Team Primakara Developers, All Right Reserved
+        </div>
+      </div>
+    </div>
+</footer>
+<!-- footer end section-->
