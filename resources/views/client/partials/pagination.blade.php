9ea6fb97eb762a29fb30fb7ba638864625ad561a
Wahyu Budi Saputra <wahyubucil@gmail.com>
2019-01-02 20:33:29 +0800

    Cleanup file naming

--- resources/views/client/partials/pagination.blade.php
+++ resources/views/client/partials/pagination.blade.php
@@ -1,24 +1,24 @@
-<div class="column-pagination">
-    <div class="column-pagination__block">
+<div class="pagination">
+    <div class="pagination__block">
         @foreach($elements as $element)
             @if(is_array($element))
                 @foreach($element as $page => $url)
                     @php
                         if($page == 1){
-                            $specialClass = "column-pagination__block__number column-pagination__block__number--only";
+                            $specialClass = "pagination__block__number pagination__block__number--only";
                         }
                     @endphp
 
                     @if($page == $paginator->currentPage())
-                        <a class="{{ $specialClass }} column-pagination__block__number column-pagination__block__number--active">{{ $page }}</a>
+                        <a class="{{ $specialClass }} pagination__block__number pagination__block__number--active">{{ $page }}</a>
                     @else
-                        <a href="{{ $url }}" class="{{ $specialClass }} column-pagination__block__number">{{ $page }}</a>
+                        <a href="{{ $url }}" class="{{ $specialClass }} pagination__block__number">{{ $page }}</a>
                     @endif
                 @endforeach
             @endif
 
             @if(is_string($element))
-                <a class="column-pagination__block__number"><span class="column-pagination__block__number--dots">...</span></a>
+                <a class="pagination__block__number"><span class="pagination__block__number--dots">...</span></a>
             @endif
         @endforeach
     </div>
