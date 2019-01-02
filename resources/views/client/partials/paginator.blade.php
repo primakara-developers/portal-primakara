cddb43bfe3abb4e62a7641dd45133a9c6eb2c542
dodypras <prasetyodody17@gmail.com>
2019-01-02 16:24:50 +0800

    update logic for paginator on category page

--- resources/views/client/partials/paginator.blade.php
+++ resources/views/client/partials/paginator.blade.php
@@ -1,23 +1,25 @@
- @if($paginator->hasPages())
- <div class="column-pagination">
+<div class="column-pagination">
     <div class="column-pagination__block">
-        @foreach($elements as $element)
-            @if(is_array($element))
-                @foreach($element as $page => $url)
-                    @if($page == 1)
-                        <a href="#" class="column-pagination__block__number column-pagination__block__number--only">1</a>
-                    @elseif($page == $paginator->currentPage())
-                        <a href="#" class="column-pagination__block__number column-pagination__block__number--active">{{ $page }}</a>
-                    @else
-                        <a href="{{ $url }}" class="column-pagination__block__number">{{ $page }}</a>
-                    @endif
-                @endforeach
-            @endif
+        @if($paginator->hasPages())
+            @foreach($elements as $element)
+                @if(is_array($element))
+                    @foreach($element as $page => $url)
+                        @if($page == 1)
+                            <a href="#" class="column-pagination__block__number column-pagination__block__number--only">1</a>
+                        @elseif($page == $paginator->currentPage())
+                            <a href="#" class="column-pagination__block__number column-pagination__block__number--active">{{ $page }}</a>
+                        @else
+                            <a href="{{ $url }}" class="column-pagination__block__number">{{ $page }}</a>
+                        @endif
+                    @endforeach
+                @endif
 
-            @if(is_string($element))
-                <a href="#" class="column-pagination__block__number"><span class="column-pagination__block__number--dots">...</span></a>
-            @endif
-        @endforeach
+                @if(is_string($element))
+                    <a href="#" class="column-pagination__block__number"><span class="column-pagination__block__number--dots">...</span></a>
+                @endif
+            @endforeach
+        @else
+            <a href="#" class="column-pagination__block__number column-pagination__block__number--only column-pagination__block__number column-pagination__block__number--active">1</a>
+        @endif
     </div>
 </div>
-@endif
